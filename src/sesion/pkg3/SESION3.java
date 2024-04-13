85be9698092372036f9452b23c44955f584f28d8
Marco Garcia <118572129+JMarco-Garcia@users.noreply.github.com>
2024-04-13 12:56:55 -0500

    Add files via upload

---
+++ src/sesion/pkg3/SESION3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion.pkg3;
+
+import Sesion3Entidades.Persona;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class SESION3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona per = new Persona();
+        per.setNombre("Juan");
+        per.setApellido("Diaz");
+        per.setDni("00055555");
+        per.setDireccion("Av Manzanitas 456");
+        per.setTelefono("991847595");
+        per.setCorreo("juan.diaz0upn.pe");
+        per.setUsuario("juanchis16");
+        per.setContraseña("123456");
+        per.setEmail("juan.diaz@gmail.com");
+        
+    }
+    
+}
