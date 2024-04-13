47003208d946ca6fb3cac8eca0ea517ca0a35217
Marco Garcia <118572129+JMarco-Garcia@users.noreply.github.com>
2024-04-13 13:04:29 -0500

    version 2

--- src/Sesion3Entidades/Persona2.java
+++ src/Sesion3Entidades/Persona2.java
@@ -9,8 +9,39 @@ package Sesion3Entidades;
  * @author Estudiante
  */
 public class Persona2 {
-    
-    
-    
-    
+
+    private PersonaDatosBasicos personaB;
+    private PersonaDatosContacto personaC;
+    private PersonaDatosRegistro personaR;
+
+    public Persona2(PersonaDatosBasicos personaB, PersonaDatosContacto personaC, PersonaDatosRegistro personaR) {
+        this.personaB = personaB;
+        this.personaC = personaC;
+        this.personaR = personaR;
+    }
+
+    public PersonaDatosBasicos getPersonaB() {
+        return personaB;
+    }
+
+    public PersonaDatosContacto getPersonaC() {
+        return personaC;
+    }
+
+    public PersonaDatosRegistro getPersonaR() {
+        return personaR;
+    }
+
+    public void setPersonaB(PersonaDatosBasicos personaB) {
+        this.personaB = personaB;
+    }
+
+    public void setPersonaC(PersonaDatosContacto personaC) {
+        this.personaC = personaC;
+    }
+
+    public void setPersonaR(PersonaDatosRegistro personaR) {
+        this.personaR = personaR;
+    }
+
 }
